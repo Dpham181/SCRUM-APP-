87801cdbd86f0422f1926b0b8e30aeb9c02fdd91
dpham171 <danhpham312@gmail.com>
2021-09-24 02:14:54 -0700

    homepage layout

--- web-app/app.js
+++ web-app/app.js
@@ -1,5 +1,4 @@
-"use strict" 
-
+"use strict"
 
 const createError = require('http-errors');
 const express = require('express');
@@ -13,13 +12,14 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, "node_modules/mdb-ui-kit/css")));
 
 app.use('/', indexRouter);
 

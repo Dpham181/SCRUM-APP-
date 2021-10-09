f49c63943c3cc28ee59e384a496a107194b98ad5
student <danhpham312@gmail.com>
2021-10-09 11:42:46 -0700

    register/style/profileupdate

--- web-api/user.py
+++ web-api/user.py
@@ -9,8 +9,17 @@ import sqlite3
 def createUser(UsersDB):
     statement = '''INSERT INTO users(UserName,PassWord) VALUES(:UserName, :PassWord)'''
     rep = root.PostMethod(UsersDB,statement,{'UserName','PassWord'})
+   
+    
     return rep
+#signup profile 
+@route('/Users/Profile/', method='POST')
+def createUserProfile(UsersDB):
+    stmt = ''' INSERT INTO Profiles(PUSER_ID,FIRST_NAME,LAST_NAME,COUNTRY,ZIPCODE,CITY,STREET,STATE) VALUES (:PUSER_ID,F:FIRST_NAME,:LAST_NAME,:COUNTRY,:ZIPCODE,:CITY,:STREET,:STATE) '''
 
+    rep = root.PostMethod(UsersDB,stmt,{'PUSER_ID','FIRST_NAME','LAST_NAME','COUNTRY','ZIPCODE','CITY','STREET','STATE'})
+   
+    return rep
 #authenticate user
 #http --verbose POST localhost:5100/auth/ UserName="DANH" PassWord="123"
 
@@ -25,4 +34,12 @@ def checkPassword(UsersDB):
         return HTTPResponse({"User_id":userpassword['User_id']},200)
     return HTTPResponse({'Authentication':False},401)
 
-
+# user profile 
+@route('/Users/Profile', method='GET')
+def ListUserTeam(UsersDB):
+    User_id = request.query.id
+    Profile = root.query(UsersDB, 'select FIRST_NAME,LAST_NAME,COUNTRY,ZIPCODE,CITY,STREET,STATE from Profiles where PUSER_ID =?',[User_id])
+    if not Profile:
+        abort(400)
+    rep = {'Profile': Profile}    
+    return HTTPResponse(rep,200)
